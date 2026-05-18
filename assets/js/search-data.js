c345d3ac5d425a785c98ad7a1d3fb156359b7a49
ccnoecker <ccnoecker@gmail.com>
2026-05-18 19:48:12 +0000

    Deploying to gh-pages from @ ccnoecker/ccnoecker.github.io@cac553d9073afd0dfb71541963204143a5152fe1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,26 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather.html";
-            },},{id: "projects-project-1",
+        },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
